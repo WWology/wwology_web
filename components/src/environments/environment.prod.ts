a7da569a410632ea7d6fc3c14d53e3e885cebdc8
wwology <ww@wwology.tech>
2020-10-13 13:15:08 +0700

    Added Components Folder

---
+++ components/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDeEnIDPXUp9pqXUDxgs4sZLhuZeyUmoYQ',
+    authDomain: 'wwology-9f4ba.firebaseapp.com',
+    databaseURL: 'https://wwology-9f4ba.firebaseio.com',
+    projectId: 'wwology-9f4ba',
+    storageBucket: 'wwology-9f4ba.appspot.com',
+    messagingSenderId: '37298019179',
+    appId: '1:37298019179:web:78bfcba3c8784937208a2e',
+    measurementId: 'G-G24GY719R8',
+  },
+};
---
+++ components/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDeEnIDPXUp9pqXUDxgs4sZLhuZeyUmoYQ',
+    authDomain: 'wwology-9f4ba.firebaseapp.com',
+    databaseURL: 'https://wwology-9f4ba.firebaseio.com',
+    projectId: 'wwology-9f4ba',
+    storageBucket: 'wwology-9f4ba.appspot.com',
+    messagingSenderId: '37298019179',
+    appId: '1:37298019179:web:78bfcba3c8784937208a2e',
+    measurementId: 'G-G24GY719R8',
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
