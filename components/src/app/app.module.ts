fc55b1698ab61793f3ccdba29ebefb55796b04d6
wwology <ww@wwology.tech>
2020-10-13 18:06:43 +0700

    Angular Elements Configured to work with Hugo

--- components/src/app/app.module.ts
+++ components/src/app/app.module.ts
@@ -1,18 +1,41 @@
+// Angular Core
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+// Firebase
+import { AngularFireModule } from '@angular/fire';
+import {
+  AngularFirePerformanceModule,
+  PerformanceMonitoringService,
+} from '@angular/fire/performance';
+import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
+import { environment } from 'src/environments/environment';
 
+const comps = [];
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [...comps],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirePerformanceModule,
+    AngularFireAnalyticsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: comps,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const elements: any[] = [];
+
+    for (const [component, name] of elements) {
+      const el = createCustomElement(component, { injector: this.injector });
+      customElements.define(name, el);
+    }
+  }
+}
